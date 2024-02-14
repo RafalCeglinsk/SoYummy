cd02dc04858d184b30e939420f1297a42e66b1f5
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-02-14 14:41:32 +0100

    Test Render

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,14 @@
+import React from 'react';
+
+import { SocialMediaBar } from 'components/SocialMediaBar/SocialMediaBar';
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+     <p>FONT text TEST</p>
+      <SocialMediaBar/>
     </div>
+    
   );
 };
