3e2f967d5218cf95c9077f095ea509f935bf50ef
RafalCeglinsk <123418749+RafalCeglinsk@users.noreply.github.com>
2024-02-15 22:38:49 +0100

    Update app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Routes, Route } from 'react-router-dom';
 import AddRecipePage from './pages/AddRecipePage';
-import { SocialMediaBar } from 'components/SocialMediaBar/SocialMediaBar';
 
 export const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/add" element={<AddRecipePage />} />
-      </Routes>
-      <div>
-        <p>FONT text TEST</p>
-        <SocialMediaBar />
-      </div>
-    </Router>
+    <Routes>
+      <Route path="/add" element={<AddRecipePage />} />
+    </Routes>
   );
 };
+
+// W PLIKU APP.JSX ZNAJDUJA SIE TYLKO KOMPONENTY, KTÓRE SĄ WYKORZYSTYWANE W PLIKU INDEX.JS
+// NIE PISZEMY KODU W APP.JSX
