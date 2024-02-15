79bf95c4facec070d6b62b67c449be8057342aa4
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-02-15 15:56:17 +0100

    Style ChooseYrBreakfast

--- Frontend/src/components/MainPageHiro/ChooseYourBreakfast/ChooseYourBreakfast.styled.jsx
+++ Frontend/src/components/MainPageHiro/ChooseYourBreakfast/ChooseYourBreakfast.styled.jsx
@@ -1,12 +1,22 @@
 import styled from 'styled-components';
+
+import BgImg from "../../../images/MainPageHero/ChooseYour1x.png"
+
+
+
 export const ContentContainer = styled.div`
-  /* tu będzie zdjęcie sałaty i cały kontener w formie absolute do którego będzie orientowany to białe okno  */
   position: relative;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-image: url(${BgImg});
+
 `;
 
 export const TextContainer = styled.div`
+
   background-color: var(--color-bg-modal);
-  color:var(--color-text-2);
+  color: var(--color-text-2);
   font-weight: 500;
   font-size: 12px;
   position: absolute;
@@ -14,23 +24,21 @@ export const TextContainer = styled.div`
   right: 7px;
   max-width: 225px;
   border-radius: 8px;
-  padding:10px;
+  padding: 10px;
 
-  & span{
-    color:var(--color-text-5);
+  & span {
+    color: var(--color-text-5);
   }
 `;
 export const LinkRecipies = styled.a`
-    
-display: flex;
-justify-content: flex-end;
-color:var(--color-text-2);
-transition: color var(--transition-time) var(--cubic);
-align-items: center;
+  display: flex;
+  justify-content: flex-end;
+  color: var(--color-text-2);
+  transition: color var(--transition-time) var(--cubic);
+  align-items: center;
 
-
-&:hover, &:focus {
+  &:hover,
+  &:focus {
     color: var(--color-text-4);
-
   }
-`
+`;
