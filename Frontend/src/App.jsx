1d0b9452071b1ab9ffb57f6085267e821743cb7d
MikolajBajdalski <bajdalm033@gmail.com>
2024-02-15 21:25:22 +0100

    fixed app.jsx and index.js

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AddRecipePage from './pages/AddRecipePage';
+// import { SocialMediaBar } from 'components/SocialMediaBar/SocialMediaBar';
 
 export const App = () => {
   return (
@@ -8,6 +9,10 @@ export const App = () => {
       <Routes>
         <Route path="/add" element={<AddRecipePage />} />
       </Routes>
+      <div>
+        <p>FONT text TEST</p>
+        {/* <SocialMediaBar /> */}
+      </div>
     </Router>
   );
 };
